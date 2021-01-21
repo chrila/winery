53d12b926afacb2b56ecb98fec3e1863c071f4b8
Christian Lampl <christian.lampl@gmx.at>
2021-01-20 22:06:06 -0300

    Add: readme, seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,10 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# create some sample grapes
+Grape.create('Carmenere')
+Grape.create('Cabernet Sauvignon')
+Grape.create('Malbec')
+Grape.create('Zweigelt')
+Grape.create('Gewürztraminer')
+Grape.create('Merlot')
+Grape.create('Syrah')
+Grape.create('Sauvignon blanc')
+Grape.create('Chardonnay')
