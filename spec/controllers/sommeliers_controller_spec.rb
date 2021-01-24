c48a84eac0f6e0d8314c1d230b58583e42b0d3e0
Christian Lampl <christian.lampl@gmx.at>
2021-01-24 11:47:38 -0300

    Add: rspec for SommeliersController

--- spec/controllers/sommeliers_controller_spec.rb
+++ spec/controllers/sommeliers_controller_spec.rb
@@ -2,4 +2,25 @@ require 'rails_helper'
 
 RSpec.describe SommeliersController, type: :controller do
 
+  login_user
+
+  describe 'GET index' do
+    it 'returns a successful response' do
+      get :index
+      expect(response).to be_successful
+    end
+
+    it 'assigns @sommeliers' do
+      sommelier = Sommelier.create(name: 'Kurt Cobain', age: 27, nationality: 'USA')
+      sommeliers = Sommelier.all
+      get :index
+      expect(assigns(:sommeliers)).to match_array(sommeliers)
+    end
+
+    it 'renders the index template' do
+      get :index
+      expect(response).to render_template('index')
+    end
+  end
+
 end
