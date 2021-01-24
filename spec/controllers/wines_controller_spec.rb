f68a34ba6c714fc57bd5209b5c396ab98ebfa0de
Christian Lampl <christian.lampl@gmx.at>
2021-01-24 11:45:32 -0300

    Add: rspec for WinesController

--- spec/controllers/wines_controller_spec.rb
+++ spec/controllers/wines_controller_spec.rb
@@ -2,4 +2,25 @@ require 'rails_helper'
 
 RSpec.describe WinesController, type: :controller do
 
+  login_user
+
+  describe 'GET index' do
+    it 'returns a successful response' do
+      get :index
+      expect(response).to be_successful
+    end
+
+    it 'assigns @wines' do
+      wine = Wine.create(name: 'Mountain Blend')
+      wines = Wine.all
+      get :index
+      expect(assigns(:wines)).to match_array(wines)
+    end
+
+    it 'renders the index template' do
+      get :index
+      expect(response).to render_template('index')
+    end
+  end
+
 end
